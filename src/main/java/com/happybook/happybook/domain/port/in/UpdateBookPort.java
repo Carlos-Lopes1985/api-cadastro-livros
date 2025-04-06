a4ee01182626ee94f710482feaf96b038e770be8
brunocesarteixeira1 <brunocesarteixeira1@gmail.com>
2025-04-06 18:27:26 -0300

    Adicionando soma e subtração

--- src/main/java/com/happybook/happybook/domain/port/in/UpdateBookPort.java
+++ src/main/java/com/happybook/happybook/domain/port/in/UpdateBookPort.java
@@ -1,8 +1,9 @@
 package com.happybook.happybook.domain.port.in;
 
+import com.happybook.happybook.domain.enums.Operation;
 import com.happybook.happybook.domain.model.Book;
 
 public interface UpdateBookPort {
 
-    Book updateQuantity(Long id, Long quantity);
+    Book updateQuantity(Long id, Long quantity, Enum<Operation> Operation);
 }
